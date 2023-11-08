f981bc0d283d988afda0636093f88ee3fe30c96d
Gemmuel <gemmueldelacruz@gmail.com>
2023-11-08 22:16:13 +0800

    update: client api

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://linkgrove-server.onrender.com";
 
 export const fetchAccountUser = async () => {
     const response = await axios.get(`${API_URL}/users/details`, {
